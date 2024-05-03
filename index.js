deba4b94bceab7059bfaa93a5aff995f310329df
gfrdhgdd <hsjchhhamburger@hotmail.com>
2024-05-04 00:49:59 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const initializeArrayWithValues = (n, value = 0) => Array(n).fill(value);
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
