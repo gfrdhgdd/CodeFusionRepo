cb6fb3c977e7b2c65a60b7aa3399a533670226b4
gfrdhgdd <hsjchhhamburger@hotmail.com>
2024-05-05 19:03:07 +0700

    commit

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-function isAlienSorted(words, order) {
-  const dict = new Map();
-  for (let i = 0; i < order.length; i++) {
-    dict.set(order[i], i);
-  }
-  for (let i = 0; i < words.length - 1; i++) {
-    const word1 = words[i];
-    const word2 = words[i + 1];
-    let found = false;
-    for (let j = 0; j < Math.min(word1.length, word2.length); j++) {
-      if (word1[j] !== word2[j]) {
-        if (dict.get(word1[j]) > dict.get(word2[j])) return false;
-        found = true;
-        break;
+function maximalSquare(matrix) {
+  if (matrix.length === 0 || matrix[0].length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from({ length: rows + 1 }, () =>
+    Array.from({ length: cols + 1 }, () => 0),
+  );
+  let maxSquare = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+        maxSquare = Math.max(maxSquare, dp[i][j]);
       }
     }
-    if (!found && word1.length > word2.length) return false;
   }
-  return true;
+  return maxSquare * maxSquare;
 }
